99e36374f630ba829842ac239bb64857bd1adac5
Rostyslav Zhabinskyi <zhabinskiirostislav@gmail.com>
2021-01-03 13:53:00 +0200

    added Redux-Todos-Example

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
 
-const App = () => {
-  return <div></div>;
-};
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer);
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
